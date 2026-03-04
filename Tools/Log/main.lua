053d2d8069c85b68ac1484869245d5d48d62925e
Vran_n <103916699+Vran-n@users.noreply.github.com>
2026-03-04 18:33:26 +0800

    Update to Log 1.1
    - Any instance will now display its proper full name (i.e., their ancestry)
    - Empty tables no longer create new lines for visual clarity
    - yellow() will no longer output a bolded text

--- Tools/Log/main.lua
+++ Tools/Log/main.lua
@@ -2,50 +2,134 @@
 -- by Vran
 -- https://polytoria.com/u/Vran
 
------ Log v1.0
+----- Log v1.1
 -- A simple utility script for better outputs. Supports tables
 
+--              MM/DD/YYYY
+----- CHANGELOG (3/4/2026)
+-- Any instance will now display its proper full name (i.e., their ancestry)
+-- Empty tables no longer create new lines for visual clarity
+-- .yellow() will no longer output a bolded text
 
+
+
+local function GetFullName(instance)
+    local Ancestry = {instance}
+
+    local function GetParent(instance)
+        local par = instance.Parent
+        if not par or par.ClassName == "Game" then return end
+        table.insert(Ancestry, 1, par)
+        GetParent(par)
+    end; GetParent(instance)
+
+    local str = ''
+    for i, ins in ipairs(Ancestry) do
+        str = i ~= #Ancestry and str..ins.Name..'.' or str..ins.Name
+    end
+
+    return str
+end
+
+-- i got really lazy here and im sorry
 local function tostring_tbl (table_s)
     if type(table_s) ~= "table" then return print('<b><color=orange>[WARN]: '.. tostring(table_s)..' is a '..type(table_s)..', NOT a table' ..'</color></b>') end
-    
-    local function converter(table_t, indent)
-        local str = ''
-        local indent = indent or 5
+
+    local function ToArray(table_t)
         local Array = {}
 
         for i, v in next, table_t do
             table.insert(Array, {[i] = v})
         end
 
+        return Array
+    end
+    
+    local function converter(table_t, indent)
+        local str = ''
+        local indent = indent or 5
+        local Array = ToArray(table_t)
+
         for i, tbl in next, Array do
             for i_name, v in next, tbl do
-                if type(v) == "table" then
+                if type(v) == "table" then      -- value is a table
                     str = i ~= #Array 
+                        and #ToArray(v) > 0     -- put a comma if i is not the last index and the value is not empty
                         and string.format(
                             '%s\n%s[%s] = {%s\n%s},', 
                             str, 
                             string.rep(" ", indent), 
-                            type(i_name) == "string" and string.format('"%s"', i_name) or tostring(i_name), 
+
+                            type(i_name) == "string" and string.format('"%s"', i_name)
+                            or type(i_name) == "userdata" and GetFullName(i_name) 
+                            or tostring(i_name),
+
                             converter(v, indent + 5),
                             string.rep(" ", indent)
-                        )
+                        ) 
+
                         or string.format(
+                            '%s\n%s[%s] = {},', 
+                            str, 
+                            string.rep(" ", indent), 
+
+                            type(i_name) == "string" and string.format('"%s"', i_name)
+                            or type(i_name) == "userdata" and GetFullName(i_name) 
+                            or tostring(i_name)
+                        ) 
+
+
+
+                        and #ToArray(v) > 0     -- don't put a comma if i is the last index and the value is not empty
+                        and string.format(
                             '%s\n%s[%s] = {%s\n%s}', 
                             str, 
                             string.rep(" ", indent), 
-                            type(i_name) == "string" and string.format('"%s"', i_name) or tostring(i_name),
+
+                            type(i_name) == "string" and string.format('"%s"', i_name) 
+                            or type(i_name) == "userdata" and GetFullName(i_name) 
+                            or tostring(i_name),
+
                             converter(v, indent + 5),
                             string.rep(" ", indent)
                         )
-                else
+
+                        or string.format(
+                            '%s\n%s[%s] = {}', 
+                            str, 
+                            string.rep(" ", indent), 
+
+                            type(i_name) == "string" and string.format('"%s"', i_name) 
+                            or type(i_name) == "userdata" and GetFullName(i_name) 
+                            or tostring(i_name)
+                        ) 
+                else                            -- value is not a table
                     str = i ~= #Array
-                        and string.format('%s\n%s[%s] = %s,', str, string.rep(" ", indent), 
-                            type(i_name) == "string" and string.format('"%s"', i_name) or tostring(i_name),
-                            type(v) == "string" and string.format('"%s"', v) or tostring(v))
-                        or string.format('%s\n%s[%s] = %s', str, string.rep(" ", indent), 
-                            type(i_name) == "string" and string.format('"%s"', i_name) or tostring(i_name),
-                            type(v) == "string" and string.format('"%s"', v) or tostring(v))
+                        and string.format('%s\n%s[%s] = %s,',       -- put a comma if i is not the last index
+                            str, 
+                            string.rep(" ", indent), 
+
+                            type(i_name) == "string" and string.format('"%s"', i_name) 
+                            or type(i_name) == "userdata" and GetFullName(i_name) 
+                            or tostring(i_name),
+
+                            type(v) == "string" and string.format('"%s"', v) 
+                            or type(v) == "userdata" and GetFullName(v) 
+                            or tostring(v)
+                        )
+
+                        or string.format('%s\n%s[%s] = %s',         -- don't put a comma if i is the last index
+                            str, 
+                            string.rep(" ", indent), 
+
+                            type(i_name) == "string" and string.format('"%s"', i_name) 
+                            or type(i_name) == "userdata" and GetFullName(i_name) 
+                            or tostring(i_name),
+
+                            type(v) == "string" and string.format('"%s"', v) 
+                            or type(v) == "userdata" and GetFullName(v) 
+                            or tostring(v)
+                        )
                 end
             end
         end
@@ -58,22 +142,38 @@ local function tostring_tbl (table_s)
 end
 
 
+
 local log = {}
+-- I will fix the boilerplate if 2.0 still doesn't do this
 
 function log.warn(...)
     local arg = {...}
     for i, k in ipairs(arg) do
-        arg[i] = type(k)=='string' and k or type(k)=='table' and tostring_tbl(k) or tostring(k)
+        arg[i] = type(k) =='userdata' 
+                and GetFullName(k)
+                    or type(k)=='string' 
+                    and k 
+                        or type(k)=='table' 
+                        and tostring_tbl(k) 
+                            or tostring(k) 
+                
         arg[i] = i == 1 and '<b><color=orange>[WARN]: '.. arg[i] ..'</color></b>' or '<b><color=orange>'.. arg[i] ..'</color></b>'
     end
     print(table.unpack(arg))
 end
 
--- A severe warning
+-- A severe warning (Usecase: An error without stoping the script)
 function log.crit(...)
     local arg = {...}
     for i, k in ipairs(arg) do
-        arg[i] = type(k)=='string' and k or type(k)=='table' and tostring_tbl(k) or tostring(k)
+        arg[i] = type(k) =='userdata' 
+                and GetFullName(k)
+                    or type(k)=='string' 
+                    and k 
+                        or type(k)=='table' 
+                        and tostring_tbl(k) 
+                            or tostring(k) 
+
         arg[i] = i == 1 and '<b><color=red>[CRITICAL]: '.. arg[i] ..'</color></b>' or '<b><color=red>'.. arg[i] ..'</color></b>'
     end
     print(table.unpack(arg))
@@ -82,17 +182,32 @@ end
 function log.important(...)
     local arg = {...}
     for i, k in ipairs(arg) do
-        arg[i] = type(k)=='string' and k or type(k)=='table' and tostring_tbl(k) or tostring(k)
+        arg[i] = type(k) =='userdata' 
+                and GetFullName(k)
+                    or type(k)=='string' 
+                    and k 
+                        or type(k)=='table' 
+                        and tostring_tbl(k) 
+                            or tostring(k) 
+
         arg[i] = i == 1 and '<b><color=green>[IMPORTANT]: '.. arg[i] ..'</color></b>' or '<b><color=green>'.. arg[i] ..'</color></b>'
     end
     print(table.unpack(arg))
 end
 
+-- Print but in sickass yellow
 function log.yellow(...)
     local arg = {...}
     for i, k in ipairs(arg) do
-        arg[i] = type(k)=='string' and k or type(k)=='table' and tostring_tbl(k) or tostring(k)
-        arg[i] = '<b><color=yellow>'.. arg[i] ..'</color></b>'
+        arg[i] = type(k) =='userdata' 
+                and GetFullName(k)
+                    or type(k)=='string' 
+                    and k 
+                        or type(k)=='table' 
+                        and tostring_tbl(k) 
+                            or tostring(k) 
+
+        arg[i] = '<color=yellow>'.. arg[i] ..'</color>'
     end
     print(table.unpack(arg))
 end
@@ -101,7 +216,13 @@ end
 function log.info(...)
     local arg = {...}
     for i, k in ipairs(arg) do
-        arg[i] = type(k)=='string' and k or type(k)=='table' and tostring_tbl(k) or tostring(k)
+        arg[i] = type(k) =='userdata' 
+                and GetFullName(k)
+                    or type(k)=='string' 
+                    and k 
+                        or type(k)=='table' 
+                        and tostring_tbl(k) 
+                            or tostring(k) 
     end
     print(table.unpack(arg))
 end
